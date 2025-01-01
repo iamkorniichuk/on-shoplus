c78de51843d211764f96a0dad1c6bf55a7d768a2
iamkorniichuk <iamkorniichuk@gmail.com>
2025-01-01 21:18:05 +0200

    Add shop search

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import login_view
+from .views import *
 
 
 app_name = "frontend"
 
 urlpatterns = [
     path("login/", login_view, name="login"),
+    path("shops/", shops_view, name="shops"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from shoplus import COUNTRY_CODES
+
 
 def login_view(request):
     return render(request, "users/login.html")
+
+
+def shops_view(request):
+    context = {
+        "countries": COUNTRY_CODES,
+    }
+    return render(request, "shops/shops.html", context=context)
