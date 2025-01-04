3f0f410d14878a86e2040cbb6598b98a1b9b8406
DmitrijGnatyuk <ipz213_gds@student.ztu.edu.ua>
2025-01-04 11:19:30 +0200

    Add display table

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import login_view, signup_view
+from .views import login_view, signup_view, shops_list_view
 
 
 app_name = "frontend"
@@ -8,4 +8,5 @@ app_name = "frontend"
 urlpatterns = [
     path("login/", login_view, name="login"),
     path("signup/", signup_view, name="signup"),
+    path("shops/search/<int:id>", shops_list_view, name="shops-list"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -16,3 +16,11 @@ def signup_view(request):
         "url": reverse("users:signup"),
     }
     return render(request, "users/auth_form.html", context=context)
+
+def shops_list_view(request, id):
+    context = {
+        "title": "Shops",
+        #"url": reverse("shops:search", args=[id])
+        "url": f"/api/shops/search/{id}",
+    }
+    return render(request, "shops/display_table.html", context=context)
