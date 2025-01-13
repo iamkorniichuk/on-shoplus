dfb458695b4785a3b68d23640a20f82323b46f95
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2025-01-13 18:54:55 +0200

    Move stripe's credentials to .env

--- on_shoplus/settings/credentials.py
+++ on_shoplus/settings/credentials.py
@@ -10,8 +10,10 @@ env.read_env("credentials.env")
 
 if DEBUG:
     stripe.api_key = env.str("STRIPE_TEST_PRIVATE_KEY")
+    STRIPE_PUBLISHABLE_KEY = env.str("STRIPE_TEST_PUBLISHABLE_KEY")
 else:
     stripe.api_key = env.str("STRIPE_LIVE_PRIVATE_KEY")
+    STRIPE_PUBLISHABLE_KEY = env.str("STRIPE_LIVE_PUBLISHABLE_KEY")
 
 
 SHOPLUS_USERNAME = env.str("SHOPLUS_USERNAME")
