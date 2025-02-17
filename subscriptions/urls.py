bd27e366cd243ebc3c1db151fb1c7b3bb9da3f0f
iamkorniichuk <iamkorniichuk@gmail.com>
2025-02-17 19:32:29 +0300

    Fix paid invoice webhook

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from .routers import SubscriptionRouter
 from .viewsets import SubscriptionViewSet
-from .views import InvoicePaidWebhook
+from .views import invoice_paid_webhook
 
 
 router = SubscriptionRouter()
 router.register("", SubscriptionViewSet, "")
 
 urlpatterns = [
-    path("paid-webhook/", InvoicePaidWebhook.as_view(), name="invoice-paid"),
+    path("paid-webhook/", invoice_paid_webhook, name="invoice-paid"),
 ] + router.urls
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,8 +1,10 @@
 from django.conf import settings
+from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework import views, status
-from rest_framework.response import Response
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 
 import stripe, stripe.error
 
@@ -10,34 +12,36 @@ import stripe, stripe.error
 User = get_user_model()
 
 
-class InvoicePaidWebhook(views.APIView):
-    def post(self, request):
-        event = None
+@require_POST
+@csrf_exempt
+def invoice_paid_webhook(request):
+    event = None
 
-        endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
-        signature = request.headers.get("stripe-signature")
-        try:
-            event = stripe.Webhook.construct_event(
-                request.data, signature, endpoint_secret
-            )
-        except stripe.error.SignatureVerificationError:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+    payload = request.body
+    signature = request.META["HTTP_STRIPE_SIGNATURE"]
+    endpoint_secret = settings.STRIPE_PAID_WEBHOOK_ENDPOINT_SECRET
+    try:
+        event = stripe.Webhook.construct_event(payload, signature, endpoint_secret)
+    except stripe.error.SignatureVerificationError:
+        return HttpResponse(status=status.HTTP_401_UNAUTHORIZED)
 
-        if event and event["type"] == "invoice.paid":
-            invoice = event["data"]["object"]
-            customer_id = invoice.customer
-            user = get_object_or_404(User, stripe_id=customer_id)
+    if not event or event["type"] != "invoice.payment_succeeded":
+        return HttpResponse(status=status.HTTP_406_NOT_ACCEPTABLE)
 
-            referrer = user.referrer
-            if not referrer or not referrer.account_stripe_id:
-                return Response(status=status.HTTP_200_OK)
+    invoice = event["data"]["object"]
+    customer_id = invoice["customer"]
+    user = get_object_or_404(User, stripe_id=customer_id)
 
-            amount = invoice.amount_paid // 10
-            currency = invoice.currency
-            account_id = referrer.account_stripe_id
-            stripe.Transfer.create(
-                amount=amount,
-                currency=currency,
-                destination=account_id,
-            )
-            return Response(status=status.HTTP_201_CREATED)
+    referrer = user.referrer
+    if not referrer or not referrer.account_stripe_id:
+        return HttpResponse(status=status.HTTP_200_OK)
+
+    amount = invoice.amount_paid // 10
+    currency = invoice.currency
+    account_id = referrer.account_stripe_id
+    stripe.Transfer.create(
+        amount=amount,
+        currency=currency,
+        destination=account_id,
+    )
+    return HttpResponse(status=status.HTTP_201_CREATED)
