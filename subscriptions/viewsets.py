2baeef257b52b04320b39141fd4ea6e9fb1b543c
iamkorniichuk <iamkorniichuk@gmail.com>
2025-02-18 22:14:31 +0300

    Fix subscription endpoints

--- subscriptions/viewsets.py
+++ subscriptions/viewsets.py
@@ -1,9 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status, mixins, permissions
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from knox.auth import TokenAuthentication
-from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 import stripe
 
@@ -16,7 +16,7 @@ PRICE_STRIPE_ID = "price_1QgSKgCD4T1ii2MY91Rm3Dkq"
 
 
 class SubscriptionViewSet(
-    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
     mixins.CreateModelMixin,
     mixins.DestroyModelMixin,
     GenericViewSet,
@@ -37,34 +37,54 @@ class SubscriptionViewSet(
         serializer.is_valid(raise_exception=True)
         payment_method = serializer.data["payment_method"]
 
-        customer = stripe.Customer.create(
-            name=user.username,
-            payment_method=payment_method,
-            invoice_settings={"default_payment_method": payment_method},
-        )
-        subscription = stripe.Subscription.create(
-            customer=customer,
-            items=[{"price": PRICE_STRIPE_ID}],
-        )
+        if user.stripe_id:
+            customer = stripe.Customer.retrieve(user.stripe_id)
+            customer.update(
+                {
+                    "payment_method": payment_method,
+                    "invoice_settings": {"default_payment_method": payment_method},
+                }
+            )
+        else:
+            customer = stripe.Customer.create(
+                name=user.username,
+                payment_method=payment_method,
+                invoice_settings={"default_payment_method": payment_method},
+            )
+            user.stripe_id = customer.id
+            user.save()
 
-        user.stripe_id = customer.id
-        user.save()
+        obj = Subscription.objects.filter(user=user).first()
+        if obj:
+            subscription = stripe.Subscription.retrieve(obj.stripe_id)
+            if not subscription:
+                obj.delete()
+                obj = None
 
-        instance = Subscription.objects.create(user=user, stripe_id=subscription.id)
-        serializer = self.get_serializer(instance)
+        if not obj:
+            subscription = stripe.Subscription.create(
+                customer=customer,
+                items=[{"price": PRICE_STRIPE_ID}],
+            )
+            obj = Subscription.objects.create(user=user, stripe_id=subscription.id)
+
+        serializer = self.get_serializer(obj)
         return Response(
             serializer.data,
             status=status.HTTP_201_CREATED,
         )
 
-    def retrieve(self, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
+    def list(self, request):
+        user = request.user
+        obj = get_object_or_404(Subscription, user=user)
+        serializer = self.get_serializer(obj)
         return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
-        obj = self.get_object()
-        if obj:
-            stripe.Subscription.cancel(obj.stripe_id)
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        user = request.user
+        obj = get_object_or_404(Subscription, user=user)
+        stripe.Subscription.modify(
+            obj.stripe_id,
+            cancel_at_period_end=True,
+        )
+        return Response(status=status.HTTP_204_NO_CONTENT)
